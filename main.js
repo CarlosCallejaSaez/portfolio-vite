f1f4c61afbbf7279e5b1fc7c3ea9fef3f3fada7b
CarlosCallejaSaez <carloscallejasez@gmail.com>
2023-05-19 13:15:53 +0200

    css added

--- main.js
+++ main.js
@@ -6,8 +6,7 @@ const data = {
   job: "Web Developer",
   github: "https://github.com/CarlosCallejaSaez",
   linkedin: "www.linkedin.com/CarlosCallejaSaez",
-  description:
-    `Profesional capacitado y versátil especializado en la creación y el mantenimiento de sitios web 
+  description: `Profesional capacitado y versátil especializado en la creación y el mantenimiento de sitios web 
     y aplicaciones web. Mi principal objetivo es diseñar,
      desarrollar e implementar soluciones tecnológicas que sean atractivas, funcionales y eficientes.`,
 };
@@ -100,7 +99,7 @@ const renderProjects = () => {
   divHomeHTML.innerHTML = "";
   divProjectsHTML.innerHTML = ` 
   
-<ul id="ul-projects">
+<ul class="ul-projects">
 <li><a href="${projects[0].web}"> ${projects[0].description}<img src="${projects[0].image}" alt="${projects[0].description}"></a></li>
 <li><a href="${projects[1].web}"> ${projects[1].description}<img src="${projects[1].image}" alt="${projects[1].description}"></a></li>
 <li><a href="${projects[2].web}"> ${projects[2].description}<img src="${projects[2].image}" alt="${projects[2].description}"></a></li>
@@ -117,11 +116,11 @@ const renderExperience = () => {
   divHomeHTML.innerHTML = "";
   divExperienceHTML.innerHTML = ` 
   <ul id="experience-content">
-  <li><a href="https://www.empresa1.com"><h2>Astro Tech</h2><img src="assets/empresa1.webp" alt=""><h3>2015-2018</h3><p>Sector Aeroespacial</p><p>Junior</p></a></li>
-  <li><a href="https://www.empresa2.com"><h2>Taylor</h2><img src="assets/empresa2.jpeg" alt=""><h3>2018-2019</h3><p>Sector Moda</p><p>Junior</p></a></li>
-  <li><a href="https://www.empresa3.com"><h2>Yardita Servies</h2><img src="assets/empresa3.jpg" alt=""><h3>2019-2022</h3><p>Sector Servicios</p><p>Senior</p></a></li>
-  <li><a href="https://www.empresa4.com"><h2>People</h2><img src="assets/empresa4.png" alt=""><h3>2022-2023</h3><p>Redes Sociales</p><p>Product Manager</p></a></li>
-  <li><a href="https://www.empresa5.com"><h2>Starlight</h2><img src="assets/empresa5.webp" alt=""><h3>2023-actualidad</h3><p>Implemetación Internet en zonas desfavorecidas</p><p>Product Manager</p></a></li>
+  <li><a href="https://www.empresa1.com"><h2 class="h2-exp">Astro Tech</h2><img class="img-experience" src="assets/empresa1.webp" alt=""><h3>2015-2018</h3><p>Sector Aeroespacial</p><p>Junior</p></a></li>
+  <li><a href="https://www.empresa2.com"><h2 class="h2-exp" >Taylor</h2><img  class="img-experience" src="assets/empresa2.jpeg" alt=""><h3>2018-2019</h3><p>Sector Moda</p><p>Junior</p></a></li>
+  <li><a href="https://www.empresa3.com"><h2 class="h2-exp" >Yardita Servies</h2><img class="img-experience" src="assets/empresa3.jpg" alt=""><h3>2019-2022</h3><p>Sector Servicios</p><p>Senior</p></a></li>
+  <li><a href="https://www.empresa4.com"><h2 class="h2-exp" >People</h2><img  class="img-experience" src="assets/empresa4.png" alt=""><h3>2022-2023</h3><p>Redes Sociales</p><p>Product Manager</p></a></li>
+  <li><a href="https://www.empresa5.com"><h2 class="h2-exp" >Starlight</h2><img class="img-experience" src="assets/empresa5.webp" alt=""><h3>2023-actualidad</h3><p>Implemetación Ciberseguridad</p><p>Product Manager</p></a></li>
 
 </ul>
  `;
