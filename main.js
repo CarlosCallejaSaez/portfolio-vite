43cd936b4b2bfd2541fb103d122f30c5daa2bd69
CarlosCallejaSaez <carloscallejasez@gmail.com>
2023-05-23 11:41:27 +0200

    google fonts

--- main.js
+++ main.js
@@ -116,13 +116,14 @@ const renderExperience = () => {
   divHomeHTML.innerHTML = "";
   divExperienceHTML.innerHTML = ` 
   <ul id="experience-content">
-  <li><a href="https://www.empresa1.com"><h2 class="h2-exp">Astro Tech</h2><img class="img-experience" src="assets/empresa1.webp" alt=""><h3>2015-2018</h3><p>Sector Aeroespacial</p><p>Junior</p></a></li>
-  <li><a href="https://www.empresa2.com"><h2 class="h2-exp" >Taylor</h2><img  class="img-experience" src="assets/empresa2.jpeg" alt=""><h3>2018-2019</h3><p>Sector Moda</p><p>Junior</p></a></li>
-  <li><a href="https://www.empresa3.com"><h2 class="h2-exp" >Yardita Servies</h2><img class="img-experience" src="assets/empresa3.jpg" alt=""><h3>2019-2022</h3><p>Sector Servicios</p><p>Senior</p></a></li>
-  <li><a href="https://www.empresa4.com"><h2 class="h2-exp" >People</h2><img  class="img-experience" src="assets/empresa4.png" alt=""><h3>2022-2023</h3><p>Redes Sociales</p><p>Product Manager</p></a></li>
-  <li><a href="https://www.empresa5.com"><h2 class="h2-exp" >Starlight</h2><img class="img-experience" src="assets/empresa5.webp" alt=""><h3>2023-actualidad</h3><p>Implemetación Ciberseguridad</p><p>Product Manager</p></a></li>
+  <li><div class="card"><a class="a-card" href="https://www.empresa1.com"><h2 class="h2-exp">Astro Tech</h2><img class="img-experience" src="assets/empresa1.webp" alt=""><h3>2015-2018</h3><p>Sector Aeroespacial</p><p>Junior</p></a></div></li>
+  <li><div class="card"><a class="a-card" href="https://www.empresa2.com"><h2 class="h2-exp" >Taylor</h2><img  class="img-experience" src="assets/empresa2.jpeg" alt=""><h3>2018-2019</h3><p>Sector Moda</p><p>Junior</p></a></div></li>
+  <li><div class="card"><a class="a-card" href="https://www.empresa3.com"><h2 class="h2-exp" >Yardita Servies</h2><img class="img-experience" src="assets/empresa3.jpg" alt=""><h3>2019-2022</h3><p>Sector Servicios</p><p>Senior</p></a></div></li>
+  <li><div class="card"><a class="a-card" href="https://www.empresa4.com"><h2 class="h2-exp" >People</h2><img  class="img-experience" src="assets/empresa4.png" alt=""><h3>2022-2023</h3><p>Redes Sociales</p><p>Product Manager</p></a></div></li>
+  <li><div class="card"><a class="a-card" href="https://www.empresa5.com"><h2 class="h2-exp" >Starlight</h2><img class="img-experience" src="assets/empresa5.webp" alt=""><h3>2023-actualidad</h3><p>Implemetación Ciberseguridad</p><p>Product Manager</p></a></div></li>
 
 </ul>
+
  `;
 };
 
